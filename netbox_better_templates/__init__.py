431446e2cee3b62a17ca840513660c3e792f51b4
Mohammad Heydari <HMohammad2520@Gmail.com>
2025-03-09 11:23:19 +0330

    Working Tested

--- netbox_better_templates/__init__.py
+++ netbox_better_templates/__init__.py
@@ -3,10 +3,17 @@ from netbox.plugins import PluginConfig
 class NetboxBetterTemplatesConfig(PluginConfig):
     name = 'netbox_better_templates'
     verbose_name = 'Better Templates'
-    description = 'Adds some functionality to NetBox templates and config render.'
+    description = 'Adds new functionality to NetBox config templates.'
     author = 'radin-system'
     author_email = 'technical@rsto.ir'
     version = '1.0.0'
     base_url = 'better-templates'
+
+
+    def ready(self):
+        # Apply the monkey patch when the app is ready
+
+        self.add_custom_field()
+        from . import monkey_patches
 
 config = NetboxBetterTemplatesConfig
--- netbox_better_templates/template_extensions/datetime_extension.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def datetime_extension(context):
-    """
-    Adds the `datetime` module to the Jinja2 template context.
-    """
-    return {
-        'datetime': datetime,
-    }
